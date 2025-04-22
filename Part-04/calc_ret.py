7e711a9e5442af9726edb8c9366b23f4adf8158c
Mashood-DevOps <mashood61199@gmail.com>
2025-04-22 12:24:31 +0530

    Create calc_ret.py

---
+++ Part-04/calc_ret.py
@@ -0,0 +1,15 @@
+def add(num1, num2):
+ add = num1 + num2
+ return add
+
+def sub(num1, num2):
+  sub = num1 - num2
+  return sub
+
+def mul(num1, num2):
+  mul = num1 * num2
+  return mul
+
+print(add(10, 5)) 
+x = sub(10, 5)
+print(x)
