373edab3233f7ab039f156b1db5f3b13bb7f4d18
Mashood-DevOps <mashood61199@gmail.com>
2025-04-22 12:14:02 +0530

    Create calc_fun.py

---
+++ Part-04/calc_fun.py
@@ -0,0 +1,18 @@
+num1 = 10
+num2 = 5
+
+def add():
+  add = num1 + num2
+  print(add)
+
+def sub():
+  sub = num1 - num2
+  print(sub)
+
+def mul():
+  mul = num1 * num2
+  print(mul)
+
+add()
+sub()
+mul()
