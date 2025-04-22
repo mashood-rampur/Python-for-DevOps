a78c3866f5d87df80812440d999e77c77270f166
Mashood-DevOps <mashood61199@gmail.com>
2025-04-22 12:20:30 +0530

    Update calc_in.py

--- Part-04/calc_in.py
+++ Part-04/calc_in.py
@@ -1,1 +1,15 @@
+def add(num1, num2):
+  add = num1 + num2
+  print(add)
 
+def sub(num1, num2):
+  sub = num1 - num2
+  print(sub)
+
+def mul(num1, num2):
+  mul = num1 * num2
+  print(mul)
+
+add(10, 5)
+sub(10, 5)
+mul(10, 5)
