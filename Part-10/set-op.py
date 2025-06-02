67e78d14bf7a9c26e354f1bda2c8f3523b1e2728
Mashood-DevOps <mashood61199@gmail.com>
2025-06-02 20:04:31 +0530

    Create set-op.py

---
+++ Part-10/set-op.py
@@ -0,0 +1,10 @@
+set1 = {2, 4, 6, 8}
+set2 = {6, 8, 10, 12}
+
+union_set = set1.union(set2)
+intersection_set = set1.intersection(set2)
+difference_set = set1.difference(set2)
+
+print(union_set)
+print(intersection_set)
+print(difference_set)
