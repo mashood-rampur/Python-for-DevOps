ca4c421542f865e2c07630ba57f5b0a4c699fcba
Mashood-DevOps <mashood61199@gmail.com>
2025-06-02 21:40:35 +0530

    Create index.py

---
+++ Part-10/index.py
@@ -0,0 +1,3 @@
+students_info = [{"name": "mashood", "age": "25", "class": "10"}, {"name": "arjun", "age": "35", "class": "12"}, {"name": "karan", "age": "40", "class": "11"}]
+
+print(students_info[1]["age"])
