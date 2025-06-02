b08dfb7050a76d977936cbc8d8625f940c849e64
Mashood-DevOps <mashood61199@gmail.com>
2025-06-02 19:05:51 +0530

    Create key-exist.py

---
+++ Part-10/key-exist.py
@@ -0,0 +1,5 @@
+my_dict = {"name" : "Mashood", "Age": "25", "Class": "10"}
+if "Occupation" in my_dict:
+    print("Occupation is present in the dictionary")
+else:
+    print("Occupation is not present in the dictionary")
